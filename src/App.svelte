80b465647923335972d6ab1f2eeae9c9110cbc4d
Marco Almeida <marco.almeida@ua.pt>
2023-05-09 12:40:49 +0100

    sign up new member fix, member list added

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
         <div class="shadow-none p-3 m-3 text-bg-success rounded"><a href="#" class="nav-link" style="display: block" on:click={()=>main_tab="admin"}>Admin</a></div>
         <div class="shadow-none p-3 m-3 bg-light rounded"><a href="#" class="nav-link" style="display: block" on:click={()=>main_tab="definicoes"}>Definições</a></div>
         <div class="shadow-none p-3 m-3 bg-light rounded"><a href="#" class="nav-link" style="display: block" on:click={()=>main_tab="pagamentos"}>Pagamentos</a></div>
-        <div class="shadow-none p-3 m-3 bg-light rounded"><a href="#" class="nav-link" style="display: block">Publicar</a></div>
+        <div class="shadow-none p-3 m-3 text-bg-primary rounded"><a href="#" class="nav-link" style="display: block">Publicar</a></div>
     </div>
     <div style="height: 100%; width: 80%; position: absolute; top: 0; right: 0;">
         {#if main_tab=="home"}
