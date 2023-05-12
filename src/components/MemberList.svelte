c259b8b0e5cb0f1db30cb43116d921acb36505bf
Marco Almeida <marco.almeida@ua.pt>
2023-05-12 16:30:03 +0100

    refactor

--- src/components/MemberList.svelte
+++ src/components/MemberList.svelte
@@ -1,70 +1,39 @@
 <script context="module">
-    const commonProperties = {
-        address: "Morada",
-        phoneNumber: "Telemovel/Telefone",
-        email: "Endereço de Email",
-        birthDate: "Data de nascimento",
-        sex: "Masculino/Feminino/Outro",
-        bio: "Uma biografia muito longa.",
-        image: "static/default_avatar.png",
-    };
-
-    export const nameList = [
-        Object.assign({}, commonProperties, {
-            name: "Marco António Alves Almeida",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Daniel Alexandre Martins da Silva",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Adalberto Júnior Trindade Vaz do Rosário",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Maria Julia Nunes",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "João Vitor da Rosa",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Ana Lídia da Mota",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "João Guilherme da Paz",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Luis Gustavo Azevedo",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Gustavo Henrique Teixeira",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Maria Clara Barros",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Pedro Lucas da Rosa",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "João Miguel da Rocha",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Vitor Gabriel Nascimento",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Ana Laura Barros",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Alexandre Tomás Rodrigues",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Bernardo Gonçalves",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Diogo Branco Silva",
-        }),
-        Object.assign({}, commonProperties, {
-            name: "Bruno Ferreira Gomes",
-        }),
+    export const personList = [
+        "Marco António Alves Almeida",
+        "Daniel Alexandre Martins da Silva",
+        "Adalberto Júnior Trindade Vaz do Rosário",
+        "Maria Julia Nunes",
+        "João Vitor da Rosa",
+        "Ana Lídia da Mota",
+        "João Guilherme da Paz",
+        "Luis Gustavo Azevedo",
+        "Gustavo Henrique Teixeira",
+        "Maria Clara Barros",
+        "Pedro Lucas da Rosa",
+        "João Miguel da Rocha",
+        "Vitor Gabriel Nascimento",
+        "Ana Laura Barros",
+        "Alexandre Tomás Rodrigues",
+        "Bernardo Gonçalves",
+        "Diogo Branco Silva",
+        "Bruno Ferreira Gomes",
     ];
+
+    export const nameList = [];
+    for (let x in personList) {
+        let firstNameLastName = getFirstAndLastName(personList[x]);
+        nameList.push({
+            name: personList[x],
+            address: "Morada de " + firstNameLastName,
+            phoneNumber: "Telemovel/Telefone de " + firstNameLastName,
+            email: "Endereço de Email de " + firstNameLastName,
+            birthDate: "Data de Nascimento de " + firstNameLastName,
+            sex: "Sexo de " + firstNameLastName,
+            bio: "Biografia de " + firstNameLastName,
+            image: "static/default_avatar.png",
+        });
+    }
 </script>
 
 <script>
@@ -82,6 +51,18 @@
                 .toLowerCase()
                 .includes(searchTerm)
         );
+    }
+
+    function getFirstAndLastName(fullName) {
+        // Split the full name into an array of substrings
+        const nameParts = fullName.trim().split(" ");
+
+        // Get the first name and last name from the array
+        const firstName = nameParts[0] || "";
+        const lastName = nameParts[nameParts.length - 1] || "";
+
+        // Return the first name and last name as an object
+        return firstName + " " + lastName;
     }
 </script>
 
